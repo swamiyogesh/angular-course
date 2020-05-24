bf824b7318f82e9d69f2dbf45cce3c98df8c8420
Yogesh Swami <swamiyogesh219@gmail.com>
2020-05-24 13:07:43 +0530

    module-3 added

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,107 +1,80 @@
 (function() {
-  "use strict";
+    'use strict';
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('APIBasePath', 'https://davids-restaurant.herokuapp.com')
+        .directive('foundItems', FoundItems);
 
-  angular.module("NarrowItDownApp", [])
-    .controller("NarrowItDownController", NarrowItDownController)
-    .service("MenuSearchService", MenuSearchService)
-    .directive("foundItems", FoundItemsDirective)
-    .constant("ApiBasePath", "https://davids-restaurant.herokuapp.com");
-
-  /*** CONTROLLERS ***********************************************************/
-  NarrowItDownController.$inject = ["MenuSearchService"];
-  function NarrowItDownController(MenuSearchService) {
-    var ctrl = this;
-    ctrl.searchFor = "";
-    ctrl.searchResult = ""; //Set to empty string if all OK
-    ctrl.found = [];
-
-    /*--- Methods -----------------------------------------------------------*/
-    ctrl.search = function() {
-      //If textbox not empty - do search...
-      if(ctrl.searchFor && ctrl.searchFor.length > 0) {
-        ctrl.searchResult = "";
-        var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchFor);
-        
-        promise.then(function(result) {
-          ctrl.found = result;
-          if(ctrl.found.length === 0) {
-            ctrl.searchResult = "Nothing found (matching \"" + ctrl.searchFor + "\")";
-          }
-        });
-      }
-      else
-      {
-        ctrl.searchResult = "Nothing found";
-      }
-    };
-
-    ctrl.dontWant = function(index) {
-      console.log("Index: ", index);
-      ctrl.found.splice(index, 1);
-    };
-  }
-
-  /*** SERVICES **************************************************************/
-  MenuSearchService.$inject = ["$http", "ApiBasePath"];
-  function MenuSearchService($http, ApiBasePath) {
-    var service = this;
-
-    //TODO - Cache menu list....
-    // service.getMenuItems = function() {
-    //   return $http({
-    //     method: "GET",
-    //     url: (ApiBasePath + "/menu_items.json")
-    //   });
-    // };
-
-    service.getMatchedMenuItems = function(searchTerm) {
-      return $http({
-        method: "GET",
-        url: (ApiBasePath + "/menu_items.json")
-      })
-        .then(function(response){
-          var menuItems = response.data;
-          var foundItems = filterOnDescription(menuItems.menu_items, searchTerm);
-
-          return foundItems;
-        });
-    };
-
-    //Return list where desciption contains searchTerm
-    function filterOnDescription(list, searchTerm) {
-      var newList = [];
-      
-      for(var i = 0; i < list.length; i++) {
-        if(list[i].description.indexOf(searchTerm) > 0) {
-          newList.push(list[i]);
-        }
-      }
-
-      return newList;
+    function FoundItems() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            restrict: 'E',
+            scope: {
+                items: '<',
+                onRemove: '&'
+            },
+            controller: FoundItemsDirectiveController,
+            controllerAs: 'narrowIt',
+            bindToController: true
+        };
+        return ddo;
     }
-  }
 
-  /*** DIRECTIVES ************************************************************/
-  function FoundItemsDirective() {
-    var ddo = {
-      // template: "<h2>{{ title }}</h2>"
-      //   + "<ul><li ng-repeat='item in list'>"
-      //   + "<button class='btn btn-danger pull-right' ng-click='dontWant({ index: $index });'>Don't want this one!</button>"
-      //   +"<b>{{ item.name }} [{{ item.short_name }}]</b><br />"
-      //   + "Content: {{ item.description }}</li></ul>"
-      //   + "<p>{{ result }}</p>",
-      templateUrl: "itemList.html",
-      scope: {
-        list: "<",
-        title: "@title",
-        result: "@result",
-        dontWant: "&"
-      },
-      // controller: FoundItemsDirectiveController,
-      // controllerAs: "list",
-      // bindToController: true
-    };
+    function FoundItemsDirectiveController() {
+        var narrowIt = this;
+        narrowIt.isEmptyItems = function() {
+            if (narrowIt.items !== undefined && narrowIt.items.length === 0) {
+                return true;
+            }
+            return false;
+        };
+    }
 
-    return ddo;
-  }
+    NarrowItDownController.$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+        var narrowIt = this;
+        narrowIt.narrowItDown = function() {
+            var promise = MenuSearchService.getMatchedMenuItems(narrowIt.search);
+            promise.then(function(result) {
+                narrowIt.found = result;
+            }).catch(function(e) {
+                console.log(e.message);
+            });
+        };
+        narrowIt.removeItem = function(itemIndex) {
+            narrowIt.found.splice(itemIndex, 1);
+        }
+    }
+    MenuSearchService.$inject = ['$http', 'APIBasePath'];
+
+    function MenuSearchService($http, APIBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+            return $http({
+                method: 'GET',
+                url: (APIBasePath + '/menu_items.json')
+            }).then(function success(result) {
+                var foundItems = [];
+                if (searchTerm !== undefined && searchTerm.length > 0) {
+                    searchTerm = searchTerm.toLowerCase();
+                    for (var i = 0; i < result.data.menu_items.length; i++) {
+                        var menu_item = result.data.menu_items[i];
+                        var description = menu_item.description.toLowerCase();
+                        if (description.indexOf(searchTerm) !== -1) {
+                            foundItems.push(menu_item);
+                        }
+                    }
+                }
+                return foundItems;
+            }, function error(response) {
+                throw new Error("Error occured!");
+            });
+
+        };
+
+    }
+
 })();
